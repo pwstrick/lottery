f66d87d86a5956993ef293c5148add4cce44a681
pingwen <pingwen@chelun.com>
2019-01-05 18:07:25 +0800

    参数配置

--- index.php
+++ index.php
@@ -15,11 +15,11 @@ if($_SERVER['REQUEST_METHOD'] == 'GET') {
     exit;
 }
 header("Content-Type:text/html;charset=utf-8");
-$json = file_get_contents('https://www.v2ex.com/api/replies/show.json?topic_id=513679');
+$json = file_get_contents('https://www.v2ex.com/api/replies/show.json?topic_id=513679');	//可配置
 $json = json_decode($json, true);
 $arr = [];
 $key = 'member_id';
-$end = mktime(23, 59, 59, 12, 6, 2018);	//截至日期 2018.12.6 23:59:59					
+$end = mktime(23, 59, 59, 1, 9, 2019);	//截至日期 2019.1.9 23:59:59（可配置）			
 //去掉重复、截至日期后的回帖和作者发的回复
 foreach($json as $k => $v) {
     if (in_array($v[$key], $arr) || $v['created'] > $end || $v['member']['username'] == 'pwstrick') {
